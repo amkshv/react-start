c63f6f84ac8a04a475204e88c0a7bd1eae346c25
amkshv <makashov.atai@gmail.com>
2019-08-02 10:22:59 +0600

    delete button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import User from './components/User';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    person: [
+      {
+        name: 'atai',
+        age: 23
+      },
+      {
+        name: 'Faisal',
+        age: 19
+      },
+      {
+        name: 'John Doe',
+        age: 30
+      },
+      {
+        name: 'Trump',
+        age: 66
+      },
+      {
+        name: 'Erbol',
+        age: 14
+      }
+    ]
+  }
+
+  onDelete = (index) => {
+    console.log()
+    let person = this.state.person;
+    person.splice(index, 1);
+    // console.log(person);
+    this.setState({ person })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        { this.state.person.map((item, index) => <User key={index} data={item} callback={() => this.onDelete(index)}/>) }
+      </div>
+    )
+    }
 }
 
 export default App;
